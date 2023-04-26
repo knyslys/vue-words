2521b106c6bcd2e5948c721b21acae6f7ca7b88b
knyslys <vakaris.serbenta7@gmail.com>
2023-04-26 17:24:16 +0300

    .

---
+++ src/components/Navigation.vue
@@ -0,0 +1,20 @@
+<template>
+  <container>
+    <nav>
+      <ul>
+        <li><router-link to="/">Home</router-link></li>
+        <li><router-link to="/about">Home</router-link></li>
+      </ul>
+    </nav>
+  </container>
+</template>
+
+<script setup>
+import Container from "./UI/Container.vue";
+</script>
+
+<style lang="scss" scoped>
+a {
+  color: white;
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,7 @@
-<script setup>
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
+<script setup></script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <div>
+    <h1>hi</h1>
+  </div>
 </template>
